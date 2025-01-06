dfabb1531d16dc3ca53bedc64425cf8316850042
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2025-01-06 01:53:53 +0100

    feat: Register CustomUserAdmin to enhance user management in admin panel

--- userAuth/admin.py
+++ userAuth/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'is_email_verified', 'date_joined')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+admin.site.register(User, CustomUserAdmin)
