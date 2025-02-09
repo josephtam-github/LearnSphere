7996081ac2a8a59d04d050eb4e73f737d338c39d
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2025-02-09 05:52:23 +0100

    Feat: add redirect to Postman documentation in root URLs

--- learnsphereBE/urls.py
+++ learnsphereBE/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/', include('userAuth.urls')),
+    path('', RedirectView.as_view(url='https://documenter.getpostman.com/view/24232846/2sAYX8KMrD', permanent=False))
 ]
