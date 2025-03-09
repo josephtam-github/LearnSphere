2c6bcc1686b4e72a8ad80e50eb1eec79cf127ed5
Joseph Tam <josephtam247@gmail.com>
2025-03-09 20:47:55 +0100

    Refactor: update admin registration to use Parent model instead of User

--- userAuth/admin.py
+++ userAuth/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import Parent
 
 class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'username', 'is_email_verified', 'date_joined')
     search_fields = ('email', 'username')
     ordering = ('email',)
 
-admin.site.register(User, CustomUserAdmin)
+admin.site.register(Parent, CustomUserAdmin)
