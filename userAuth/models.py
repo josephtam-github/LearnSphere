09422d0bbe9f52b5292964684225cbe595f6c524
jamesunekwuojo <unekwuojoabah4@gmail.com>
2025-05-13 00:01:21 +0100

    working on django signals

--- userAuth/models.py
+++ userAuth/models.py
@@ -10,6 +10,8 @@ import random
 
 from django.utils.timezone import now, timedelta
 
+from django.conf import settings
+
 import secrets
 
 class Parent(AbstractUser):
@@ -42,11 +44,12 @@ class Parent(AbstractUser):
         self.otp_code = str(random.randint(100000, 999999))
         self.otp_expiry = now() + timedelta(minutes=5)
         self.save()
+    
      
-class otpToken(models.Model):
+class OtpToken(models.Model):
     """Model to store OTP tokens for users(parents)"""
     
-    user = models.ForeignKey(Parent, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     otp_code = models.CharField(max_length=6, default=secrets.token_hex(3))
     otp_created_at = models.DateTimeField(auto_now_add=True)
     otp_expires_at = models.DateTimeField(blank=True, null=True)
